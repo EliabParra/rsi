ea25ca530e15673bdc9455d8dc0343c0069d9abf
EliabParra <eliabparra@gmail.com>
2026-05-17 20:12:25 -0400

    added config file for ips and ports

---
+++ shared/config.js
@@ -0,0 +1,28 @@
+import { getLocalIP } from "./getLocalIP"
+
+export const config = {
+    dispatcher: {
+        host: getLocalIP(),
+        port: 3000
+    },
+    boServers: {
+        calculator: {
+            host: getLocalIP(),
+            port: 4001
+        },
+        equation: {
+            host: getLocalIP(),
+            port: 4002
+        }
+    },
+    clients: {
+        client1: {
+            host: getLocalIP(),
+            port: 5001
+        },
+        client2: {
+            host: getLocalIP(),
+            port: 5002
+        }
+    }
+}
