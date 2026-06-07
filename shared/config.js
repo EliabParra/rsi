b3d4407553dc267f005b08bcc018e6c8652b35ac
arrias24 <andresarriasc@gmail.com>
2026-06-07 18:44:00 -0400

    chore(config): update shared configuration parameters for distributed system

--- shared/config.js
+++ shared/config.js
@@ -1,26 +1,38 @@
-import { getLocalIP } from "./getLocalIP.js"
+import { getLocalIP } from "./getLocalIP.js";
 
 export const config = {
-    dispatcher: {
-        host: getLocalIP(),
-        port: 3000
+  dispatcher: {
+    host: getLocalIP(),
+    port: 3000,
+  },
+  boServers: {
+    calculator: [{ id: "calc-1", host: "172.20.243.176", port: 4001 }],
+    equation: [{ id: "eq-1", host: "172.20.243.176", port: 4002 }],
+  },
+  clients: {
+    client1: {
+      host: "172.20.243.244",
+      port: 5001,
     },
-    boServers: {
-        calculator: [
-            { id: 'calc-1', host: '172.20.243.176', port: 4001 }
-        ],
-        equation: [
-            { id: 'eq-1', host: '172.20.243.176', port: 4002 }
-        ]
+    client2: {
+      host: "172.20.243.244",
+      port: 5002,
     },
-    clients: {
-        client1: {
-            host: '172.20.243.244',
-            port: 5001
-        },
-        client2: {
-            host: '172.20.243.244',
-            port: 5002
-        }
-    }
-}
+  },
+  heartbeat: { interval: 2000, deadTimeout: 6000 },
+
+  loadBalancer: {
+    maxCores: 1,
+    maxSpeed: 1,
+    maxTotalMem: 1,
+    maxInFlight: 1,
+    maxRps: 1,
+
+    wsCores: 0.2,
+    wsSpeed: 0.2,
+    wsMem: 0.1,
+    wdInFlight: 0.3,
+    wdRps: 0.2,
+    interval: 2000,
+  },
+};
